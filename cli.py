77f194dac24fe51a1fb41885252bc3bd4f0dfdbd
Andrew Scheller <andrew.scheller@raspberrypi.com>
2024-08-13 00:36:38 +0100

    Make output file an explicit argument. (because relying on output-redirection meant that on failure, it still "sucessfully" created a zero-byte output file, which messed up later Make steps) Also update requirements.txt

--- cli.py
+++ cli.py
@@ -8,28 +8,28 @@ from .nodes import Node, DoxygenindexNode
 
 
 def main():
-    """Convert the given Doxygen index.xml to AsciiDoc and print the result."""
+    """Convert the given Doxygen index.xml to AsciiDoc and output the result."""
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", help="The path of the file to convert", default=None)
+    parser.add_argument("-o", "--output", help="The path of the output file", default=None)
     parser.add_argument("-c", "--child", help="Is NOT the root index file", default=False, action='store_true')
     args = parser.parse_args()
     filename = args.file
+    output_filename = args.output
     is_child = args.child
-    if filename:
+    if filename and output_filename:
         xmldir = os.path.dirname(filename)
         with open(filename, encoding="utf-8") as xml:
             if is_child:
-                print(
-                    Node(
-                        BeautifulSoup(xml, "xml").doxygen, xmldir=xmldir
-                    ).to_asciidoc()
-                )
+                result = Node(
+                    BeautifulSoup(xml, "xml").doxygen, xmldir=xmldir
+                ).to_asciidoc()
             else:
-                print(
-                    DoxygenindexNode(
-                        BeautifulSoup(xml, "xml").doxygenindex, xmldir=xmldir
-                    ).to_asciidoc()
-                )
-                
+                result = DoxygenindexNode(
+                    BeautifulSoup(xml, "xml").doxygenindex, xmldir=xmldir
+                ).to_asciidoc()
+        with open(output_filename, "w", encoding="utf-8") as output:
+            output.write(result)
+
     else:
         sys.exit(1)
